21972d70bf1bf5ac788d8158a65b845ac714357e
jamesfallon99 <53617404+jamesfallon99@users.noreply.github.com>
2021-07-11 13:59:50 +0100

    Add files via upload

---
+++ web_applications/README.txt
@@ -0,0 +1,4 @@
+No extras were added to this assignment.
+Attempted this assignment when the recorded lectures wouldn't work on timekeeper and before they were put on drive so researched online instead.
+Please use the following link to access the video:
+https://drive.google.com/drive/folders/1TZLAgAwv5U94SsaE-GKdcq7cRwYMRmN-?usp=sharing
--- web_applications/web_app.py
+++ web_applications/web_app.py
@@ -1,15 +1,9 @@
-from flask import Flask
-from flask import render_template
-from flask import request #Allows you to inspect the information a client sends(e.g. parameters, ip address etc)
-from flask import redirect
+from flask import Flask, redirect, request, render_template
+#request allows you to inspect the information a client sends(e.g. parameters, ip address etc)
 import json
 import csv
 
 app = Flask(__name__) #Create the application object
-
-#@app.route('/sayhello') 
-#def mygreetingt():
-    #return "Hi there"
 
 @app.route('/')
 def homepage():
@@ -31,10 +25,13 @@ def formtest(): #Display a web form asking a user for their name
         return redirect('/showname?username='+name) #The redirect function, redirects the client to the /showname page, this displays the name of the user using flask templates
     return render_template('form.html')
 
-#@app.route('/allegiances')
-#def allegiances():
 
-def csv_to_json(csvFilePath, jsonFilePath):#---part 3--rewrite myself as copied directly
+@app.route('/allegiances')#part 3
+def allegiances():
+    
+    csvFilePath = "allegiance.csv"
+    jsonFilePath = 'templates/'r'data.json'
+    
     jsonArray = []
       
     #read csv file
@@ -51,30 +48,17 @@ def csv_to_json(csvFilePath, jsonFilePath):#---part 3--rewrite myself as copied 
     with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
         jsonString = json.dumps(jsonArray, indent=4)
         jsonf.write(jsonString)
-          
-csvFilePath = "allegiance.csv"
-jsonFilePath = 'templates/'r'data.json'
-
-@app.route('/allegiances')
-def allegiances():
-    csv_to_json(csvFilePath, jsonFilePath)
     #f = open('data.json')
     #data = json.load(f)
     #print(data)
     return render_template('data.json')
 
-
-@app.route('/allegiancedashboard')
+#Fetching json data
+#To be able to display this data in our HTML file, we first need to fetch the data with JavaScript.
+#We will fetch this data by using the fetch API. We use the fetch API in the following way:
+@app.route('/allegiancedashboard')#part 4
 def allegiance_dash():
     return render_template('allegiance_dash.html')
     
 if __name__ == "__main__":
     app.run()
-
-#POST-Data that you want to send to the server stored in a packet
-#capture information from the user
-#act on that info or print it back to the user
-
-#To access form data in our route, we use request.form.
-#Let's capture our incoming form data to a variable called req, but first, we should add a conditional to validate we're receiving POST data.
-#We can do so by testing request.method for the "POST" method:
